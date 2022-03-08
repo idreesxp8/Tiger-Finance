98de0153c990904954521e089cd2f6c00f6d68e6
flopmart <34133163+asadnisar98@users.noreply.github.com>
2022-03-08 13:51:05 +0500

    new

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,7 +16,34 @@ $('.owl-news').slick({
     slidesToScroll: 3,
     margin: '10px',
     centerMode: true,
-    centerPadding: '0px'
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '180px'
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
 
   });
 
