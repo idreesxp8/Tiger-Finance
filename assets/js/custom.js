6070421bf32cce780e3237b72bde1ee73cc69dd0
Ahsan Zahoor <ahsanzahoor2@gmail.com>
2022-03-08 16:10:11 +0500

    responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,105 +1,105 @@
-$(document).ready(function(){
-
-  $('.lending_mob').slick({
+$(document).ready(function () {
+  $(".lending_mob").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: '90px',
+    centerPadding: "90px",
     responsive: [
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
+          slidesToScroll: 1,
+        },
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
+    ],
   });
-        
-   
-$('.owl-news').slick({
+
+  $(".owl-news").slick({
     infinite: true,
     slidesToShow: 3,
     dots: true,
     slidesToScroll: 3,
-    margin: '10px',
+    margin: "10px",
     centerMode: true,
-    centerPadding: '0px',
+    centerPadding: "0px",
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          centerPadding: '180px'
-        }
+          centerPadding: "180px",
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
+          slidesToScroll: 1,
+        },
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
-
+    ],
   });
 
-    if($('.brands_slider').length)
-    {
-    var brandsSlider = $('.brands_slider');
-    
-    brandsSlider.owlCarousel(
-    {
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:4000,
-    nav:false,
-    dots:false,
-    autoWidth:true,
-    items:3,
-    margin:60
+  if ($(".brands_slider").length) {
+    var brandsSlider = $(".brands_slider");
+
+    brandsSlider.owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      nav: false,
+      dots: false,
+      autoWidth: true,
+      items: 3,
+      margin: 60,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 3,
+        },
+        1000: {
+          items: 5,
+        },
+      },
     });
-    
-    if($('.brands_prev').length)
-    {
-    var prev = $('.brands_prev');
-    prev.on('click', function()
-    {
-    brandsSlider.trigger('prev.owl.carousel');
-    });
+
+    if ($(".brands_prev").length) {
+      var prev = $(".brands_prev");
+      prev.on("click", function () {
+        brandsSlider.trigger("prev.owl.carousel");
+      });
     }
-    
-    if($('.brands_next').length)
-    {
-    var next = $('.brands_next');
-    next.on('click', function()
-    {
-    brandsSlider.trigger('next.owl.carousel');
-    });
+
+    if ($(".brands_next").length) {
+      var next = $(".brands_next");
+      next.on("click", function () {
+        brandsSlider.trigger("next.owl.carousel");
+      });
     }
-    }
-    
-    
-    });
+  }
+});
